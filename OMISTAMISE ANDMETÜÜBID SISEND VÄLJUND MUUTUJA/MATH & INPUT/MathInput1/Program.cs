e8e99276e30614e00949ac01a24adf358fd2259b
Teur234 <aazzeeqqssdd94@gmail.com>
2026-01-15 17:34:13 +0200

    New year new me, new programs
    Just a few new programs

---
+++ OMISTAMISE ANDMETÜÜBID SISEND VÄLJUND MUUTUJA/MATH & INPUT/MathInput1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MathInput1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programm 1
+            //int vanus = 0;
+            //Console.WriteLine("Tere, alustamiseks palun sisestage oma vanus.");
+            //vanus = int.Parse(Console.ReadLine());
+            //int aastaarv = 0;
+            //Console.WriteLine("Jätkamiseks palun sisestage selle kalendriaasta number.");
+            //aastaarv = int.Parse(Console.ReadLine());
+            //int sünniaasta = aastaarv - vanus;
+            //Console.WriteLine($"Olete sündinud aastal {sünniaasta}.");
+
+            //Programm 2
+            //string kasutajaNimi = "";
+            //Console.WriteLine("Sisse logimiseks palun sisestage oma nimi.");
+            //kasutajaNimi = Console.ReadLine();
+            //Console.WriteLine($"Olete edukalt sisse logitud, {kasutajaNimi}");
+
+            //Programm 3
+        }
+    }
+}
